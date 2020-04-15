bd529ac3231731eea55cb719788fb3ee56f749f8
Akash <akashverma.mec18@itbhu.ac.in>
2020-04-15 18:23:03 +0000

    pt.3

--- Public/stylesheets/script.js
+++ Public/stylesheets/script.js
@@ -3,7 +3,7 @@
 $("ul").on("click","li",function(){
 	$(this).toggleClass("ltg");
 });
-$(".delbtn").
+
 $("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
